9ce58793d58f8a0260885fa176e8fa98d4bb0658
Joshua Eze <jezeadiebuo5@gmail.com>
2025-06-09 00:05:56 +0100

    working on theme to match business brand'

--- README.md
+++ README.md
@@ -1,36 +1,24 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+---
+title: REMWaste
+description: web app
+author: Joshua Eze
+created:  2025 Jun 7
+updated: 2025 Jun 8
+---
 
-## Getting Started
+REMWaste
+=========
 
-First, run the development server:
+## development
+I started with consuming the api using useEffect, then moved onto layout, UX design and finished off with responsiveness. I went with a white background because of UX considerations, as most users are familiar with neutral themes. I also chose to use Impact font because it communicates information of high importance like signage, safety messages; which applies to the nature of your business.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+## How to run the app
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Run (npm run start:dev) from the main directory to compile for development. 
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
 
-## Learn More
 
-To learn more about Next.js, take a look at the following resources:
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
 
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,13 +5,15 @@ import Card from "./ui/Card";
 
 export default function HomeComponent() {
   const [skipData, setSkipData] = React.useState<any[]>([]);
+  const [selectedSkip, setSelectedSkip] = React.useState<any>();
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const chooseSelected = (e: React.MouseEvent, id: string) => {
-    const modal = document.getElementById('progress-modal');
+    const modal = document.getElementById("progress-modal");
 
-    if(modal){
-      modal.classList.remove('translate-y-[100%]');
-      modal.classList.add('translate-y-0'); 
+    if (modal) {
+      modal.classList.remove("translate-y-[100%]");
+      modal.classList.add("translate-y-0");
     }
 
     const updatedSkipData = skipData.map((skip: any) => {
@@ -21,9 +23,11 @@ export default function HomeComponent() {
       return { ...skip, selected: false };
     });
     setSkipData(updatedSkipData);
+    setSelectedSkip(updatedSkipData.find((skip) => skip.id === id));
   };
 
   React.useEffect(() => {
+    setIsLoading(true);
     async function getSkipData() {
       const res = await fetch(
         "https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft"
@@ -31,6 +35,7 @@ export default function HomeComponent() {
 
       const data = await res.json();
       setSkipData(data);
+      setIsLoading(false);
     }
 
     getSkipData();
@@ -38,64 +43,98 @@ export default function HomeComponent() {
 
   return (
     <>
-      <main
-        role="main"
-        className="min-h-screen font-impact w-full mx-auto container space-y-12 flex flex-col pb-7 md:pt-14 pt-3 px-4 lg:pl-0 lg:pr-6"
-      >
-        <header className="space-y-3 flex w-full flex-col items-center">
-          <h1 className="md:text-3xl text-xl text-red-800">
-            Choose Your Skip Size
-          </h1>
-          <h3 className="text-[16px] text-gray-400">
-            Select the skip size that best suits your needs
-          </h3>
-        </header>
-        <section className="flex flex-wrap md:gap-10 gap-6">
-          {skipData.map((skip: any, i: number) => (
-            <Card key={i} skip={skip} handleSelect={chooseSelected}>
-              <div>
-                <h1 className="text-xl text-red-800">{skip.size} Yard Skip</h1>
-                <h3 className="text-[16px] text-gray-400">
-                  {skip.hire_period_days} day hire period
-                </h3>
-              </div>
-              <h1 className="text-xl text-red-800">
-                &#163;
-                {Number(skip.price_before_vat + skip.vat).toLocaleString(
-                  "en-GB"
+      {isLoading ? (
+        <div className="flex justify-center items-center flex-col gap-y-2 bg-white h-screen w-full md:px-16 px-8 md:pt-12 pt-5 ">
+          <h1 className="font-sans text-orange-600">getting data...</h1>
+          <span className="border-4 border-transparent rounded-full border-t-orange-600 border-r-orange-600 w-[36px] h-[36px] spin"></span>
+        </div>
+      ) : (
+        <main
+          role="main"
+          className="min-h-screen font-impact w-full mx-auto container space-y-12 flex flex-col pb-7 md:pt-14 pt-3 px-4 lg:pl-0 lg:pr-6"
+        >
+          <header className="space-y-3 flex w-full flex-col items-center">
+            <h1 className="md:text-3xl text-2xl text-orange-600">
+              Choose Your Skip Size
+            </h1>
+            <h3 className="text-[16px] text-gray-400">
+              Select the skip size that best suits your needs
+            </h3>
+          </header>
+          <section className="flex flex-wrap md:gap-10 gap-6">
+            {skipData.map((skip: any, i: number) => (
+              <Card key={i} skip={skip} handleSelect={chooseSelected}>
+                <div>
+                  <h1 className="md:text-xl text-lg text-orange-600">
+                    {skip.size} Yard Skip
+                  </h1>
+                  <h3 className="md:text-[16px] text-sm text-gray-400">
+                    {skip.hire_period_days} day hire period
+                  </h3>
+                </div>
+                <h1 className="text-xl text-orange-600">
+                  &#163;
+                  {Number(skip.price_before_vat + skip.vat).toLocaleString(
+                    "en-GB"
+                  )}
+                </h1>
+                {skip.selected ? (
+                  <button
+                    type="button"
+                    className="cursor-pointer group hover:ring-1 ring-orange-600 transition-all duration-300 ease-in-out bg-orange-600 w-full py-4 text-white rounded-lg"
+                  >
+                    Selected
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="cursor-pointer group bg-[#10131c] w-full hover:ring-1 ring-[#10131c] transition-all ease-in-out duration-300 py-4 text-white rounded-lg flex gap-2 items-center justify-center"
+                  >
+                    <span>Select This Skip</span>
+                    <i className="fa-solid fa-arrow-right transition-transform duration-300 ease-in-out relative group-hover:translate-x-1"></i>
+                  </button>
                 )}
-              </h1>
-              {skip.selected ? (
-                <button
-                  type="button"
-                  className="cursor-pointer group hover:ring-1 ring-red-800 transition-all duration-300 ease-in-out bg-red-800 w-full py-4 text-white rounded-lg"
-                >
-                  Selected
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  className="cursor-pointer group bg-gray-400 w-full hover:ring-1 ring-gray-400 transition-all ease-in-out duration-300 py-4 text-white rounded-lg flex gap-2 items-center justify-center"
-                >
-                  <span>Select This Skip</span>
-                  <i className="fa-solid fa-arrow-right transition-transform duration-300 ease-in-out relative group-hover:translate-x-1"></i>
-                </button>
-              )}
-            </Card>
-          ))}
-        </section>
-      </main>
-      <div id='progress-modal' className="py-6 md:px-10 px-8 transform transition-transform ease-in-out duration-400 text-center space-y-8 bg-gray-400 text-white translate-y-[100%] z-50 h-auto fixed bottom-0 left-0 w-full">
-        <p className="text-sm">
+              </Card>
+            ))}
+          </section>
+        </main>
+      )}
+      <div
+        id="progress-modal"
+        className="py-6 md:px-10 px-8 transform transition-transform ease-in-out duration-400 text-center space-y-8 bg-[#10131c] text-white translate-y-[100%] z-50 h-auto fixed bottom-0 left-0 w-full"
+      >
+        <p className="text-sm ">
           Imagery and information shown throughout this website may not reflect
           the exact shape or size specification, colours may vary, options
           and/or accessories may be featured at additional cost.
         </p>
-        <div className="flex flex-row items-center md:justify-between justify-center w-full">
-          <div className="inline-flex"></div>
-          <div className="inline-flex text-white gap-4">
-            <button type="button" className="rounded-lg cursor-pointer bg-white hover:ring-1 ring-white transition-all duration-300 ease-in-out px-4 py-2 text-black">Back</button>
-            <button type="button" className="bg-red-800 hover:ring-1 transtion-all duration-300 ease-in-out ring-red-800 px-5 py-2 rounded-lg cursor-pointer group flex gap-2 items-center justify-center">
+        <div className="flex font-impact md:flex-row flex-col items-center md:justify-between justify-center w-full gap-4">
+          {selectedSkip && (
+            <div className="bg-white px-3 py-2 rounded-lg text-black font-light">
+              {selectedSkip.size}&nbsp;Yard Skip&nbsp;
+              <span className="text-orange-600">
+                <span className="font-sans font-semibold">(</span>
+                <span>
+                  &#163;
+                  {Number(
+                    selectedSkip.price_before_vat + selectedSkip.vat
+                  ).toLocaleString("en-GB")}
+                </span>
+                <span className="font-sans font-semibold">)</span>
+              </span>
+            </div>
+          )}
+          <div className="inline-flex text-white gap-4 md:w-fit w-full">
+            <button
+              type="button"
+              className="md:w-fit w-[50%] rounded-lg cursor-pointer bg-white hover:ring-1 ring-white transition-all duration-300 ease-in-out px-4 py-2 text-black"
+            >
+              Back
+            </button>
+            <button
+              type="button"
+              className="bg-orange-600 md:w-fit w-[50%] hover:ring-1 transtion-all duration-300 ease-in-out ring-orange-600 px-5 py-2 rounded-lg cursor-pointer group flex gap-2 items-center justify-center"
+            >
               <span>Continue</span>
               <i className="fa-solid fa-arrow-right transition-transform duration-300 ease-in-out relative group-hover:translate-x-1"></i>
             </button>
